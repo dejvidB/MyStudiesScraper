303e475a5cec0c45221b79d0e025656d343da455
Dejvid Bushi <dejvid.bushi@gmail.com>
2023-02-19 19:54:46 +0200

    Add required history parameter

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,15 @@ import Main from './components/main';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import { Router } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 
 const store = ConfigureStore();
+const history = createBrowserHistory();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
+      <Router history={history}>
         <div className="App">
           <Main />
         </div>
